9b83b74cd49e3c8d10ae73de65581c8ce787c50b
wangchristine <c56984200123@gmail.com>
2020-08-07 00:31:29 +0800

    paginate and simple paginate for collection

---
+++ src/ApiResponseServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CHHW\ApiResponse;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class ApiResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Collection::macro('paginate', function ($perPage = 15, $pageName = 'page', $page = null) {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+            $total = $this->count();
+            $results = $this->forPage($page, $perPage)->values();
+
+            $paginator = new LengthAwarePaginator($results, $total, $perPage, $page, [
+                'path' => LengthAwarePaginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+            ]);
+
+            return $paginator;
+        });
+
+        Collection::macro('simplePaginate', function ($perPage = 15, $pageName = 'page', $page = null) {
+            $page = $page ?: Paginator::resolveCurrentPage($pageName);
+            $results = $this->slice(($page - 1) * $perPage)->take($perPage + 1)->values();
+
+            $paginator = new Paginator($results, $perPage, $page, [
+                'path' => Paginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+            ]);
+
+            return $paginator;
+        });
+    }
+}
